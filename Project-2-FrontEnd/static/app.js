e36cb9ceca58ff25de8b353a2ae0db5e28b446c6
Thomas Levy <40259992+tlevy77@users.noreply.github.com>
2021-02-22 19:28:15 -0800

    Delete Project-2-FrontEnd directory

--- Project-2-FrontEnd/static/app.js
+++
@@ -1,7 +0,0 @@
-var dropdown = d3.select("#button1")
-
-dropdown.on("click", function(event) {
-    event.append("a").classed("dropdown-item", true).attr("href", "#").text("New entry")
-});
-
-console.log(dropdown)
